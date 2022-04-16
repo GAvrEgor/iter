a1ada83f9c13d50054541f59688198caa55fa44a
Gavrilov. E <Egor4it@yandex.ru>
2022-04-16 10:36:52 +0500

    ///

--- gen/main.py
+++ gen/main.py
@@ -5,23 +5,31 @@ nested_list = [
 ]
 
 
+
 class MyIterator:
 
-	def __init__(self, nested_list):
-		self.start = -1
-		self.end = len(nested_list)
+	def __init__(self, list_):
+		self.list_= list_
+		self.len = len(nested_list)
+		self.main_list_cursor = -1
+		self.nested_list_cursor = 0
+
 
 	def __iter__(self):
+		self.main_list_cursor +=1
+		self.nested_list_cursor = 0
 		return self
 
 	def __next__(self):
-		self.start += 1
-		if self.start == self.end:
+		if self.nested_list_cursor == len(self.list_[self.main_list_cursor]):
+			iter(self)
+		if self.main_list_cursor == len(self.list_):
 			raise StopIteration
-		return self
+		self.nested_list_cursor += 1
+		return self.list_[self.main_list_cursor][self.nested_list_cursor - 1]
 
-	def __str__(self):
-		return '\n'.join(str(item) for item in nested_list[self.start])
+	# def __str__(self):
+	# 	return '\n'.join(str(item) for item in nested_list[self.start])
 
 
 def flat_generator(my_list):
@@ -29,12 +37,13 @@ def flat_generator(my_list):
 		for elem in sub_list:
 			yield elem
 
-
+flat_list = [item for item in MyIterator(nested_list)]
+print(flat_list)
 # for item in MyIterator(nested_list):
 # 		print(item)
 
 # flat_list = [",".join(str(item).split()) for item in MyIterator(nested_list)]
 # print(flat_list)
-
-for item in flat_generator(nested_list):
-	print(item)
+#
+# for item in flat_generator(nested_list):
+# 	print(item)
